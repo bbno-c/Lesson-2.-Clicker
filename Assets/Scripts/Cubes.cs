011e4473283f08b74ece010498a8fd2d05db488f
bbno-c <khorolskiy.o.s@gmail.com>
2020-03-07 03:40:11 +0300

    обнаружил, что не все фалйы комитились

---
+++ Assets/Scripts/Cubes.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubes : MonoBehaviour
+{
+    public bool flag;
+    public int temp;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Cube_entity")
+        {
+            //Debug.Log(collision.gameObject.name);
+            //Debug.Log(gameObject.name);
+            //gameObject.transform.localScale += new Vector3(1,1,1);
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        gameObject.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
+        spawner.Destroyed++;
+    }
+
+    private void Start()
+    {
+        flag = false;
+    }
+
+    private void Update()
+    {
+        if (temp != (int)spawner.delta)
+        {
+            temp = (int)spawner.delta;
+            flag = true;
+        }
+        if (flag)
+        {
+            gameObject.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
+            flag = false;
+        }
+        if (gameObject.transform.localScale.x > 5)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
