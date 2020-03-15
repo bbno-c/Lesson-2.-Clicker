7c6d3403d7023157302c5010691b382b980826f0
bbno-c <khorolsky.o.s@gmail.com>
2020-03-15 20:07:58 +0300

    - Инициализация скорости через редактор

--- Assets/Scripts/spawner.cs
+++ Assets/Scripts/spawner.cs
@@ -12,11 +12,13 @@ public class Spawner : MonoBehaviour
     [SerializeField] private int _score;
     [SerializeField] private float _delta;
     [SerializeField] private float _speed;
+    private float _startSpeed;
     private int _temp;
     private bool _flag;
 
     private void Start()
     {
+        _startSpeed = _speed;
         _displayText = _display.GetComponent<Text>();
     }
 
@@ -28,6 +30,7 @@ public class Spawner : MonoBehaviour
 
     private void Update()
     {
+        
         _delta += Time.deltaTime;
         if (_delta > _speed)
         {
@@ -61,7 +64,7 @@ public class Spawner : MonoBehaviour
                     Destroy(child.gameObject);
                 }
                 _score = 0;
-                _speed = 0;
+                _speed = _startSpeed;
             }
         }
 
